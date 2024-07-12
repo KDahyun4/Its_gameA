ae98ecc9e88825cd027e2729841b4c3a9d0d87a4
KDahyun4 <dahyun3402@gmail.com>
2024-07-12 14:16:03 +0900

    [0712,강다현,배경삽입]lotto_game파일및사진

--- lotto_game.py
+++ lotto_game.py
@@ -1,3 +1,9 @@
 import tkinter as tk
 window = tk.Tk()
+window.title("game_test")
+#배경 뽑기판
+back_img = tk.PhotoImage(file="lotto_background_1.png")
+back_label = tk.Label(window, image=back_img)
+back_label.pack()
+
 window.mainloop()
